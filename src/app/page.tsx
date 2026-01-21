69611beccb5997f29ebb09759d246467c5267f33
sundaram26 <singhsundaram2004@gmail.com>
2026-01-21 23:38:33 +0530

    Innest setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,35 @@
-// "use client";
+"use client";
 
-// import { Button } from "@/components/ui/button";
-// import { authClient } from "@/lib/auth-client";
+import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
 import { LogoutButton } from "./logout";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 
-const Page = async () => {
+const Page = () => {  
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
   
-  await requireAuth();
-  
-  const data = await caller.getUsers();
-  
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      queryClient: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions())
+      }
+    }
+  }));
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       Protected
       <div>
       {JSON.stringify(data, null, 2)}
       </div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create workflow
+      </Button>
       <LogoutButton />
     </div>
   )
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,21 @@
-import { z } from 'zod';
 import { createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 
 export const appRouter = createTRPCRouter({
-    getUsers: protectedProcedure.query(({ ctx }) => {
-      return prisma.user.findMany({
-        where: {
-          id: ctx.auth.user.id,
-        }
-      });
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    // fetch the video
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    return prisma.workflow.create({
+      data: {
+        name: "test-workflow"
+      }
     })
+  })
 });
+
 // export type definition of API
 export type AppRouter = typeof appRouter;
