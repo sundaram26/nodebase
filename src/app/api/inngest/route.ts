2a863e565ac9474d53c772f972108640fe3b581f
sundaram26 <singhsundaram2004@gmail.com>
2026-01-24 23:23:56 +0530

    Updates

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -4,9 +4,10 @@ import { helloWorld } from "@/inngest/functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
+    
     client: inngest,
     functions: [
         helloWorld
     ],
-    
+
 });
