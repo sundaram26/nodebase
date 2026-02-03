b125ac1d18928c3dfe9e224566dc8c86c92e30b5
sundaram26 <singhsundaram2004@gmail.com>
2026-02-04 00:03:30 +0530

    Workflow error resoloved

--- src/features/workflows/hooks/use-workflows.ts
+++ src/features/workflows/hooks/use-workflows.ts
@@ -5,11 +5,13 @@
 import { useTRPC } from "@/trpc/client";
 import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
+import { useWorkflowsParams } from "./use-workflows-params";
 
 export const useSuspenseWorkflows = () => {
     const trpc = useTRPC();
+    const [params] = useWorkflowsParams();
 
-    return useSuspenseQuery(trpc.workflows.getMany.queryOptions());
+    return useSuspenseQuery(trpc.workflows.getMany.queryOptions(params));
 }
 
 /**
@@ -23,7 +25,7 @@ export const useCreateWorkflow = () => {
         onSuccess: (data) => {
             toast.success(`Workflow "${data.name} created`);
             queryClient.invalidateQueries(
-                trpc.workflows.getMany.queryOptions(),
+                trpc.workflows.getMany.queryOptions({}),
             );
         },
         onError: (error) => {
