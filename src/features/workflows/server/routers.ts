762e8549fb063acf8835b142112cb86336426dcc
sundaram26 <singhsundaram2004@gmail.com>
2026-02-04 07:20:39 +0530

    Pagination and search for workflows

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -76,6 +76,10 @@ export const workflowsRouter = createTRPCRouter({
                 prisma.workflow.count({
                     where: {
                         userId: ctx.auth.user.id,
+                        name: {
+                            contains: search,
+                            mode: "insensitive"
+                        }
                     }
                 })
             ])
