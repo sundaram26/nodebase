33b49adc4225b2036e1d54c9fd0a38b319549114
sundaram26 <singhsundaram2004@gmail.com>
2026-02-06 19:24:12 +0530

    Node variable

--- src/features/executions/components/http-request/dialog.tsx
+++ src/features/executions/components/http-request/dialog.tsx
@@ -33,6 +33,10 @@ import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
+    variableName: z
+        .string()
+        .min(1, { message: "Variable name is required" })
+        .regex(/^[A-Za-z_$][A-Za-z0-9_$]*$/, {message: "Variable name must start with a letter or underscores and contains only letter, number and underscores"}),
     endpoint: z.url({ message: "Please enter a valid URL" }),
     method: z.enum(["GET", "POST", "PUT", "PATCH", "DELETE"]),
     body: z
@@ -60,6 +64,7 @@ export const HttpRequestDialog = ({
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {
+            variableName: defaultValues.variableName || "",
             endpoint: defaultValues.endpoint || "",
             method: defaultValues.method || "GET",
             body: defaultValues.body || "",
@@ -69,6 +74,7 @@ export const HttpRequestDialog = ({
     useEffect(() => {
         if (open) {
             form.reset({
+                variableName: defaultValues.variableName || "",
                 endpoint: defaultValues.endpoint || "",
                 method: defaultValues.method || "GET",
                 body: defaultValues.body || "",
@@ -76,6 +82,7 @@ export const HttpRequestDialog = ({
         }
     }, [open, defaultValues, form])
 
+    const watchVariableName = form.watch("variableName") || "myApiCall";
     const watchMethod = form.watch("method");
     const showBodyField = ["POST", "PUT", "PATCH"].includes(watchMethod);
 
@@ -98,6 +105,28 @@ export const HttpRequestDialog = ({
                         onSubmit={form.handleSubmit(handleSubmit)}
                         className="space-y-8 mt-4"
                     >
+                        <FormField
+                            control={form.control}
+                            name="variableName"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Variable Name
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="myApiCall"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        Use this name to reference the result in other nodes:{" "}
+                                        {`{{${watchVariableName}.httpResponse.data}}`}
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
                         <FormField
                             control={form.control}
                             name="method"
--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -3,6 +3,7 @@ import { NonRetriableError } from "inngest";
 import ky, {type Options as KyOptions} from "ky";
 
 type HttpRequestData = {
+    variableName?: string;
     endpoint?: string;
     method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE",
     body?: string;
@@ -19,6 +20,10 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
         throw new NonRetriableError("HTTP Request node: no endpoint configured")
     }
 
+    if (!data.variableName) {
+        throw new NonRetriableError("Variable name not configured")
+    }
+
     const result = await step.run("http-request", async () => {
         const endpoint = data.endpoint!;
         const method = data.method || "GET";
@@ -27,6 +32,9 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
         
         if (["POST", "PUT", "PATCH"].includes(method)) {
             options.body = data.body;
+            options.headers = {
+                "Content-Type": "application/json"
+            };
         }
 
         const response = await ky(endpoint, options);
@@ -35,13 +43,24 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
             ? await response.json()
             : await response.text();
         
-        return {
-            ...context,
+        const responsePayload = {
             httpResponse: {
                 status: response.status,
                 statusText: response.statusText,
                 data: responseData,
             }
+        }
+        
+        if (data.variableName) {
+            return {
+                ...context,
+                [data.variableName]: responsePayload
+            }
+        }
+
+        return {
+            ...context,
+            ...responsePayload
         }
     })
 
